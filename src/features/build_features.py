ba999919d9053f770d2e35e056f5b8bdedc69dbb
Joana <joana.pinto@xpand-it.com>
2018-11-24 20:32:24 +0000

    [UPD] minor updates

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -4,4 +4,4 @@ from sklearn.feature_extraction.text import CountVectorizer
 def bag_of_words(corpus, max_words):
     vectorizer = CountVectorizer(analyzer = "word", tokenizer = None, preprocessor = None, stop_words = None,
                                  max_features = max_words)
-    return vectorizer.fit_transform(corpus)
+    return vectorizer
