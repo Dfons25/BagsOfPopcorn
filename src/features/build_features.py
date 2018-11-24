08d53a3363992351f7b946ccbf9a3124c4230978
Joana <joana.pinto@xpand-it.com>
2018-11-24 18:20:44 +0000

    [ADD] script to remove stopwords

---
+++ src/features/build_features.py
@@ -0,0 +1,14 @@
+import clean_dara
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def bag_of_words():
+
+
+
+def main():
+    print("main")
+
+
+if __name__ == "__main__":
+    main()
